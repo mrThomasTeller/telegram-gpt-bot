9394fbb3a83a258f189e4a0e90994ac0f2b60ef5
abakharev <abakharev@sharevault.com>
2023-07-07 11:21:49 +0300

    build

--- build/src/bot-server.js
+++ build/src/bot-server.js
@@ -3,6 +3,7 @@ import TelegramConnection from './lib/TelegramConnection.js';
 import { catchError } from './lib/async.js';
 import { isCommandForBot } from './lib/tgUtils.js';
 import message from './commands/message.js';
+import ping from './commands/ping.js';
 catchError(main());
 const whiteChatsList = (process.env.WHITE_CHATS_LIST ?? '')
     .split(',')
@@ -19,16 +20,14 @@ async function main() {
                 await tg.bot.sendMessage(msg.chat.id, '😴 Бот временно отключен для технического обслуживания. Пожалуйста, попробуйте позже.');
             }
             else {
-                await message(tg, msg);
-                // const command = msg.text.split(/ |@/)[0];
-                // switch (command) {
-                //   case '/summarize':
-                //     await summarize(tg, msg);
-                //     return;
-                //   case '/ping':
-                //     await ping(tg, msg);
-                //     return;
-                // }
+                const command = msg.text.split(/ |@/)[0];
+                switch (command) {
+                    case '/ping':
+                        await ping(tg, msg);
+                        return;
+                    default:
+                        await message(tg, msg);
+                }
             }
         }
     });
